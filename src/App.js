6e6f8962d38737b1fd8fa2b21ed71f796e8a4088
Leon Daros <leondaros@gmail.com>
2020-08-17 20:02:49 -0300

    filters and search container

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="grid-container">
+        <div>
+          <div className="grid-x">
+            <div className="cell large-6">
+              <input type="search" placeholder="Search"></input>
+            </div>
+          </div>
+          <div className="grid-x">
+          <div className="cell large-4">
+            <p>Aplicação Minima</p>
+            <div class="slider" data-slider data-initial-start="200" data-end="20000">
+              <span class="slider-handle"  data-slider-handle role="slider" tabindex="100"></span>
+              <span class="slider-fill" data-slider-fill></span>
+              <input type="hidden"></input>
+            </div>
+            <p>Até R$20.000</p>
+          </div>
+          <div className="cell large-4"></div>
+          <div className="cell large-4">
+            <p>Prazo de Resgate</p>
+            <div class="slider" data-slider data-initial-start="200" data-end="20000">
+              <span class="slider-handle"  data-slider-handle role="slider" tabindex="100"></span>
+              <span class="slider-fill" data-slider-fill></span>
+              <input type="hidden"></input>
+            </div>
+            <p>Até 30 Dias Uteis</p>
+          </div>
+        </div>
+          <p>Horario Limite de aplicação 12:00</p>
+        </div>
+      </div>
     </div>
   );
 }
