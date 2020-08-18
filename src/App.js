50dd683fe72a00fb3f2cd33bc2c96598525ffd8e
Leon Daros <leondaros@gmail.com>
2020-08-17 22:02:41 -0300

    add simple table

--- src/App.js
+++ src/App.js
@@ -12,17 +12,17 @@ function App() {
             </div>
           </div>
           <div className="grid-x">
-          <div className="cell large-4">
-            <p>Aplicação Minima</p>
-            <div class="slider" data-slider data-initial-start="200" data-end="20000">
-              <span class="slider-handle"  data-slider-handle role="slider" tabindex="100"></span>
-              <span class="slider-fill" data-slider-fill></span>
-              <input type="hidden"></input>
+            <div className="cell large-4">
+              <p>Aplicação Minima</p>
+              <div class="slider" data-slider data-initial-start="200" data-end="20000">
+                <span class="slider-handle"  data-slider-handle role="slider" tabindex="100"></span>
+                <span class="slider-fill" data-slider-fill></span>
+                <input type="hidden"></input>
+              </div>
+              <p>Até R$20.000</p>
             </div>
-            <p>Até R$20.000</p>
-          </div>
-          <div className="cell large-4"></div>
-          <div className="cell large-4">
+            <div className="cell large-4"></div>
+            <div className="cell large-4">
             <p>Prazo de Resgate</p>
             <div class="slider" data-slider data-initial-start="200" data-end="20000">
               <span class="slider-handle"  data-slider-handle role="slider" tabindex="100"></span>
@@ -31,8 +31,41 @@ function App() {
             </div>
             <p>Até 30 Dias Uteis</p>
           </div>
-        </div>
+          </div>
           <p>Horario Limite de aplicação 12:00</p>
+        </div>
+      </div>
+      <hr></hr>
+      <div className="grid-container">
+        <div className="funds-table">
+          <table>
+            <thead>
+              <tr>
+                <th width="200">Fundo</th>
+                <th>Data da Cota</th>
+                <th>Mes(%)</th>
+                <th>2016(%)</th>
+                <th>12M(%)</th>
+                <th>Aplicação Minima(%)</th>
+                <th>Prazo do Resgate</th>
+                <th>Aplicar</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>Renda Fixa</tr>
+              <tr>
+                <td>COR</td>
+                <td>Fundo Nome</td>
+                <td>Data da Cota</td>
+                <td>Mes(%)</td>
+                <td>2016(%)</td>
+                <td>12M(%)</td>
+                <td>Aplicação Minima(%)</td>
+                <td>Prazo do Resgate</td>
+                <td>Aplicar</td>
+              </tr>
+            </tbody>
+          </table> 
         </div>
       </div>
     </div>
